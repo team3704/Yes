e29a03e92540da05592b346a0252790d1e5a1136
Brambles-cat <74834218+Brambles-cat@users.noreply.github.com>
2023-01-26 13:48:30 -0800

    Fixed unnecessary multiplying thing

--- src/main/java/frc/robot/commands/ButtonCmd.java
+++ src/main/java/frc/robot/commands/ButtonCmd.java
@@ -12,13 +12,14 @@ public class ButtonCmd extends CommandBase {
 
     public ButtonCmd(RotaterSub rotater, int targetPosition) {
         this.rotater = rotater;
-        this.targetPosition = (int) (targetPosition * Constants.pidMulti_TESTING);
+        this.targetPosition = targetPosition < 0 ? targetPosition : (int) (targetPosition * Constants.pidMulti_TESTING);
         addRequirements(rotater);
     }
     
     @Override
     public void execute() {
         if(targetPosition < 0) {
+            System.out.println(targetPosition);
             switch(targetPosition) {
                 case -1: rotater.zero(); break;
                 case -2: 
@@ -27,7 +28,10 @@ public class ButtonCmd extends CommandBase {
                         CommandScheduler.getInstance().schedule(RobotContainer.cmd_spinns);
                     else CommandScheduler.getInstance().cancel(RobotContainer.cmd_spinns);
                     return;
-                case -3: System.exit(18313); break;
+                case -3:
+                    System.exit(18313);
+                    System.out.println("exitting");
+                    break;
                 default: System.out.println("Uhm"); break;
             }
 
