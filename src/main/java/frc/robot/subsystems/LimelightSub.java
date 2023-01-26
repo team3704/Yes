82adefaf24dc3d77ab6c9b02a0c0c0d8fd5159d8
Brambles-cat <74834218+Brambles-cat@users.noreply.github.com>
2023-01-26 11:58:15 -0800

    Limelight Class

---
+++ src/main/java/frc/robot/subsystems/LimelightSub.java
@@ -0,0 +1,23 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableEntry;
+import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class LimelightSub extends SubsystemBase {
+    public static double
+        x = 0, y = 0, area = 0;
+    
+    private static NetworkTable table = NetworkTableInstance.getDefault().getTable("limelight");
+    private static NetworkTableEntry
+        tx = table.getEntry("tx"),
+        ty = table.getEntry("ty"),
+        ta = table.getEntry("ta");
+
+    @Override public void periodic() {
+        x = tx.getDouble(0.0);
+        y = ty.getDouble(0.0);
+        area = ta.getDouble(0.0);
+    }
+}
