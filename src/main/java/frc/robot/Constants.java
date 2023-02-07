29b58395f3c440b5fa2b447228f5637d92e857a3
Brambles-cat <74834218+Brambles-cat@users.noreply.github.com>
2023-02-07 15:54:40 -0800

    other things

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -1,7 +1,3 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
-
 package frc.robot;
 
 public final class Constants {
@@ -11,14 +7,31 @@ public final class Constants {
     idDT_br = 3,
     idDT_fl = 10,
     idDT_fr = 2,
-    idSh_shooter = 12, //7
-    idCl_climber = 8;
+    idSh_shooter = 7,//12, //7
+    idCl_climber = 8,
+    id_xbox = 1;
   
   public static final double
     pidMulti_TESTING = 50;
     //pidMulti_ARM = 50; otherwise 1
 
-  public static class OperatorConstants {
-    public static final int kDriverControllerPort = 0;
+  public static class XBoxButtons {
+    public static final int
+      A = 1,
+      X = 3,
+      B = 2,
+      Y = 4;
   }
+  /*
+   * Ycbmuctlmleycbtoial
+   * Ycbtowltmdiycbtoial
+   * 
+   * IbtasaIcbibmbpotlIhb
+   * 
+   * Ftwtowcamdaambscnbagay
+   * Ycbmuctlmleycbtoil
+   * 
+   * IbtasaIcbibmbpotlIhb
+   * by
+   */
 }
--- src/main/java/frc/robot/Dashboard.java
+++ src/main/java/frc/robot/Dashboard.java
@@ -1,24 +1,22 @@
 package frc.robot;
 
+import frc.robot.commands.ActuatorCmd;
 import frc.robot.commands.SpinTestCmd;
 import frc.robot.subsystems.LimelightSub;
 import frc.robot.subsystems.RotaterSub;
 
 import static edu.wpi.first.wpilibj.smartdashboard.SmartDashboard.*;
-import static frc.robot.RobotContainer.joystick;
 
 public class Dashboard {
     public static void initialize() {
         new Thread(() -> {
             while(true) {
+                putNumber("Actuator position", ActuatorCmd.y);
                 putNumber("LimelightX", LimelightSub.x);
                 putNumber("LimelightY", LimelightSub.y);
                 putNumber("LimelightArea", LimelightSub.area);
-                putNumber("X", joystick.getX());
-                putNumber("Y", -joystick.getY());
                 putNumber("Angle", Math.toDegrees(SpinTestCmd.p));
                 putNumber("Encoder Reading", RotaterSub.getReading());
-                putNumber("PositionErr", RotaterSub.pid.getPositionError());
                 putNumber("calculatedOutput", RotaterSub.calculated);
                 try {Thread.sleep(20);}
                 catch (InterruptedException e) {e.printStackTrace(); break;}
