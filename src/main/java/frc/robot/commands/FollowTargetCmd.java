aae245d36a09bf1a9a5eae106709c507ef85c1c8
Brambles-cat <74834218+Brambles-cat@users.noreply.github.com>
2023-02-07 15:54:03 -0800

    hehe, robot likes me

--- src/main/java/frc/robot/commands/FollowTargetCmd.java
+++ src/main/java/frc/robot/commands/FollowTargetCmd.java
@@ -2,28 +2,38 @@ package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.Robot;
-import frc.robot.subsystems.DrivetrainSubsystem;
+import static frc.robot.subsystems.LimelightSub.*;
+
 import frc.robot.subsystems.LimelightSub;
+import frc.robot.subsystems.drivetrain.SquareDrive;
 
 public class FollowTargetCmd extends CommandBase {
-    private final DrivetrainSubsystem drivetrain;
+    private final SquareDrive drivetrain;
 
-    public FollowTargetCmd(DrivetrainSubsystem drivetrain) {
+    public FollowTargetCmd(SquareDrive drivetrain) {
         this.drivetrain = drivetrain;
         addRequirements(drivetrain);
     }
 
     @Override public void initialize() {
         Robot.manualDrive = false;
+        LimelightSub.light(true);
     }
 
     @Override public void execute() {
-        if(LimelightSub.area != 0) {
-            System.out.println("WOOF WOOF!!\nBARK BARK!!!\nWOOF!!\nWOOF WOOF BARK BARK!!!");
-        }
+        double y = 0;
+        if(area == 0) {drivetrain.arcadeDrive(0, 0, 0); return;}
+        if(area < 0.45) y = -0.13;
+        else if(area > 0.75) y = 0.13;
+        else y = 0;
+        if(x > 4) drivetrain.arcadeDrive(0.16 * getXPercent(), y, 1);
+        else if(x < -4) drivetrain.arcadeDrive(-0.16 * getXPercent(), y, 1);
+        else drivetrain.arcadeDrive(0, y, 1);
     }
 
     @Override public void end(boolean interrupted) {
+        drivetrain.arcadeDrive(0, 0, 0);
+        LimelightSub.light(false);
         Robot.manualDrive = true;
     }
 }
