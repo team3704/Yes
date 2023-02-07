6fbb2c63ceb34ceb69c3ce6056260973805bb301
Brambles-cat <74834218+Brambles-cat@users.noreply.github.com>
2023-02-07 15:55:02 -0800

    YAY SOCKETS

--- src/main/java/frc/robot/Coms.java
+++ src/main/java/frc/robot/Coms.java
@@ -1,61 +1,62 @@
 package frc.robot;
 
-import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
+import java.io.DataInputStream;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Arrays;
 
 import static java.lang.System.out;
 
 public class Coms {
     private static ServerSocket s = null;
     private static Socket client = null;
-    private static BufferedInputStream input;
+    private static DataInputStream input;
     private static BufferedOutputStream output;
-    static byte recieved = 0;
-    private static byte[] buffer = new byte[3];
-
+    public static byte[] extData = new byte[3];
+    public static boolean test = false;
     public static void connect() {
-        try {
-            s = new ServerSocket(50183);
-            client = s.accept();
-            out.println("Connected");
-            input = new BufferedInputStream(client.getInputStream());
-            output = new BufferedOutputStream(client.getOutputStream());
-        }
-        catch (IOException e) {e.printStackTrace();}
-        out.println("entering");
-        while(!s.isClosed()) {
-            recieved = request()[0];
-            out.println(recieved + " from loop");
-            //RobotContainer.shooter.setOutput(MathUtil.clamp(recieved, -80, 80));
-        }
-        out.println("exiting");
+        if(s != null) return;
+        new Thread(
+            () -> {
+                try {
+                    s = new ServerSocket(51803);
+                    out.println("awaiting connection...");
+                    client = s.accept();
+                    out.println("Connected to client");
+                    input = new DataInputStream(client.getInputStream());
+                    output = new BufferedOutputStream(client.getOutputStream());
+                }
+                catch (IOException e) {e.printStackTrace();}
+                awaitData();
+            }
+        ).start();
     }
 
     public static void disconnect() {
         try {
-            if(s != null) {s.close(); out.println("Disconnected");}
-            if(client != null) client.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        recieved = 0;
+            if(s != null) {
+                s.close(); s = null; out.println("Disconnected");
+                input.close(); input = null;
+                output.close(); output = null;
+            }
+            if(client != null) client.close(); client = null;
+        } catch (IOException e) {e.printStackTrace();}
     }
 
-    public static byte[] request() {
-        try {
-            output.write(1);
-            output.flush();
-            out.println("flushed");
-            input.read(buffer);
-            out.println("successfully read value " + buffer[0]);
-        } catch (IOException e) {
-            System.out.println("check");
-            e.printStackTrace();
-            disconnect();
+    public static void awaitData() {
+        while(true) {
+            try {
+                input.readFully(extData);//.read(buffer, 0, 3);
+                out.println("successfully read " + Arrays.toString(extData));
+            } catch (IOException e) {
+                System.out.println("check");
+                //e.printStackTrace();
+                disconnect();
+                connect();
+                return;
+            } 
         }
-        return buffer;
     }
 }
