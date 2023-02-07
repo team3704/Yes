3c80d0aa7cdb66c91d1c69a76eb159a54b0bcc11
Brambles-cat <74834218+Brambles-cat@users.noreply.github.com>
2023-02-07 15:55:23 -0800

    Also can't remember

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -7,7 +7,6 @@ package frc.robot;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
-import frc.robot.subsystems.RotaterSub;
 
 /**
  * The VM is configured to automatically run this class, and to call the functions corresponding to
@@ -28,8 +27,6 @@ public class Robot extends TimedRobot {
    */
   @Override
   public void robotInit() {
-    // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
-    // autonomous chooser on the dashboard.
     robotContainer = new RobotContainer();
     Dashboard.initialize();
   }
@@ -77,8 +74,12 @@ public class Robot extends TimedRobot {
   @Override
   public void teleopInit() {
     isAutonomous = false;
-    RotaterSub.pid.reset();
-    robotContainer.scheduleCmds();
+    Coms.disconnect();
+    Coms.connect();
+    //RotaterSub.pid.reset();
+    //robotContainer.scheduleCmds();
+    //MusicCmd.init();
+    //CommandScheduler.getInstance().schedule(aCmd);
     // This makes sure that the autonomous stops running when
     // teleop starts running. If you want the autonomous to
     // continue until interrupted by another command, remove
--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -2,49 +2,70 @@ package frc.robot;
 
 import static edu.wpi.first.math.MathUtil.applyDeadband;
 
+import frc.robot.Constants.XBoxButtons;
 import frc.robot.commands.*;
 import frc.robot.subsystems.*;
+import frc.robot.subsystems.drivetrain.SquareDrive;
 import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
+import edu.wpi.first.wpilibj2.command.button.Trigger;
 
 import static edu.wpi.first.wpilibj2.command.Commands.runOnce;
+import static java.lang.System.out;
 
 public class RobotContainer {
+  public static boolean joystickControl = false;
+  private XboxController xboxController = new XboxController(Constants.id_xbox);
   private final LimelightSub limelight = new LimelightSub();
   public static Joystick joystick = new Joystick(Constants.id_stick);
   public static final ShooterSub shooter = new ShooterSub();
-  private final DrivetrainSubsystem drivetrain = new DrivetrainSubsystem();
+  private final SquareDrive drivetrain = new SquareDrive();
   private static final RotaterSub spinSub = new RotaterSub();
   public static final SpinTestCmd cmd_spinns = new SpinTestCmd(spinSub);
 
   public RobotContainer() {
     configureBindings();
-    spinSub.zero();
   }
 
   void humanControl() {
-    drivetrain.drive(
-      applyDeadband(joystick.getX(), 0.2),
-      applyDeadband(-joystick.getY(), 0.2),
-      Math.abs((joystick.getZ() - 1) / 2)
-    );
+    if(joystick != null) {
+      drivetrain.arcadeDrive(
+        applyDeadband(joystick.getX(), 0.2),
+        applyDeadband(-joystick.getY(), 0.2),
+        getStickZ()
+      );
+    } else {
+      drivetrain.tankDrive(
+        applyDeadband(xboxController.getLeftY(), 0.2),
+        applyDeadband(xboxController.getRightY(), 0.2)
+      );
+    }
   }
 
   private void configureBindings() {
-    //new JoystickButton(joystick, 7).whileTrue(new ShootCmd(shooter));
-    /*new JoystickButton(joystick, 1).whileTrue(new ButtonCmd(spinSub, -1));
-    new JoystickButton(joystick, 3).whileTrue(new ButtonCmd(spinSub, 1000));
-    new JoystickButton(joystick, 4).whileTrue(new ButtonCmd(spinSub, 1500));
-    new JoystickButton(joystick, 2).whileTrue(new ButtonCmd(spinSub, 0));
-    new JoystickButton(joystick, 5).whileTrue(new ButtonCmd(spinSub, 500));
-    new JoystickButton(joystick, 11).whileTrue(new ButtonCmd(spinSub, -3));
-    new JoystickButton(joystick, 10).whileTrue(new ButtonCmd(spinSub, -2));*/
-    //new JoystickButton(joystick, 6).whileTrue(new FollowTargetCmd(drivetrain));
-    new JoystickButton(joystick, 8).onTrue(runOnce(() -> LimelightSub.toggleLight()));
-    new JoystickButton(joystick, 9).onTrue(new MusicCmd("yes"));
-    new JoystickButton(joystick, 7).onTrue(runOnce(() -> LimelightSub.toggleCamMode()));
+    if(joystick != null) {
+      new JoystickButton(joystick, 5).toggleOnTrue(new FollowTargetCmd(drivetrain));
+      new JoystickButton(joystick, 7).onTrue(runOnce(() -> LimelightSub.toggleLight()));
+      new JoystickButton(joystick, 6).onTrue(runOnce(() -> LimelightSub.toggleCamMode()));
+      new JoystickButton(joystick, 10).onTrue(new MusicCmd("thingy.chrp"));
+      new JoystickButton(joystick, 8).onTrue(new MusicCmd("notBoring.chrp"));
+      new JoystickButton(joystick, 9).onTrue(new MusicCmd("boring.chrp"));
+      new JoystickButton(joystick, 1).onTrue(runOnce(() -> drivetrain.invert()));
+      new JoystickButton(joystick, 11).onTrue(runOnce(() -> {
+        Coms.test = !Coms.test;
+        out.println("Pico: " + Coms.test);
+      }));
+    } else if(xboxController != null) {
+      System.out.println("XboxButton Mapping");
+      new Trigger(() -> {return xboxController.getRawButton(XBoxButtons.X);})
+        .onTrue(runOnce(() -> drivetrain.invert()));
+      /*new Trigger(null).onTrue();
+      new Trigger(null).onTrue();
+      new Trigger(null).onTrue();
+      new Trigger(null).onTrue();*/
+    }
   }
 
   public void scheduleCmds() {
